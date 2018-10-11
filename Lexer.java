651e990643a35bde31db70f9d5a98420d8b1ea41
Kazunari88 <32477111+Kazunari88@users.noreply.github.com>
2018-10-11 17:58:02 +0900

    Update Lexer.java

--- Lexer.java
+++ Lexer.java
@@ -7,40 +7,46 @@ public class Lexer {
         scanner.close();
 
         String output = "ERR";
-        Pattern p = Pattern.compile("^[0-9a-f]+");
-        Pattern p2 = Pattern.compile("^[0-9A-F]+");
-        Pattern p3 = Pattern.compile("^0x[0-9a-f]+");
-        Pattern p4 = Pattern.compile("0X[0-9A-F]+");
-        Pattern p5 = Pattern.compile("(0|[1-9][0-9]*)(\\.[0-9]*)");
-        Pattern p6 = Pattern.compile("^\\.[0-9]*");
+        Pattern p = Pattern.compile("^0"); //0
+        Pattern p2 = Pattern.compile("^[1-9][0-9]*"); //10進数整数
+        Pattern p3 = Pattern.compile("^0[xX][0-9a-fA-F]+"); //16進数
+        Pattern p4 = Pattern.compile("^[0-9]*[a-fA-F][0-9a-fA-F]*"); //0xなし16進数
+        Pattern p5 = Pattern.compile("^[1-9][0-9]*\\.[0-9]*"); //小数(任意)
+        Pattern p6 = Pattern.compile("^0\\.[0-9]*"); //小数(0.任意)
+        Pattern p7 = Pattern.compile("^\\.[0-9]+"); //小数(.任意)
         Matcher match = p.matcher(input);
         if(match.find()) {
-            output = "INT" + match.group(); //10進数か16進数(小文字)の1文字以上
+            output = "INT" + match.group();
         }
 
         match = p2.matcher(input);
-        if(match.find() && match.group().length() > output.length()) {
-            output = "INT" + match.group(); //10進数か16進数(大文字)の1文字以上
+        if(match.find()) {
+            output = "INT" + match.group();
         }
 
         match = p3.matcher(input);
         if(match.find()) {
-            output = "INT" + match.group(); //16進数0x開始(小文字)
+            output = "INT" + match.group();
         }
 
         match = p4.matcher(input);
         if(match.find()) {
-            output = "INT" + match.group(); //16進数0X開始(大文字)
+            output = "INT" + match.group();
         }
 
         match = p5.matcher(input);
         if(match.find()) {
-            output = "DEC" + match.group(); //小数点を含む場合
+            output = "DEC" + match.group();
         }
 
         match = p6.matcher(input);
         if(match.find()) {
-            output = "DEC" + match.group(); //整数自由
+            output = "DEC" + match.group();
+        }
+
+        match = p7.matcher(input);
+        if(match.find()) {
+            output = "DEC" + match.group();
         }
 
         System.out.println(output);
